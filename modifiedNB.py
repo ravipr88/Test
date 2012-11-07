8e3098a58ddb1fd6f942b618acf15f448d818463
Ravi Prakash <ravipr88@gmail.com>
2012-11-07 21:06:55 +0530

    Test2

---
+++ modifiedNB.py
@@ -0,0 +1,18 @@
+import re;
+import csv;
+import time;
+import httpagentparser;
+
+
+class modifiedNB():
+    
+    def __init__(self,filename,resp_var,static_var,dynamic_var = None,file_sep = "\t",
+                 equal_prior = True,train_prop = 0.5,split = True,markov_order = 1 ):
+        self.filename = filename;
+        self.resp_var = resp_var;
+        self.static_var = static_var;
+        self.dynamic_var = dynamic_var;
+        self.file_sep = "\t";
+        self.equal_prior = equal_prior;        
+        pass;
+    
